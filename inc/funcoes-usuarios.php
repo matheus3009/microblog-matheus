7af734944f4d97096ffca06cf41d67bf63d2db61
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-05-29 16:16:02 -0300

    Create funcoes-usuarios.php

---
+++ inc/funcoes-usuarios.php
@@ -0,0 +1,14 @@
+<?php
+/* Acessando os dados da conexão ao servidor */
+require "conecta.php";
+
+function inserirUsuario($conexao, $nome, $email, $tipo, $senha){
+   $sql = "INSERT INTO usuarios (nome, email, tipo, senha)
+    VALUES('$nome', '$email', '$tipo', '$senha')";
+}
+
+
+//executando o comando no banco
+
+mysqli_query($conexao, $sql); or die(mysqli_error($conexao));
+?>
