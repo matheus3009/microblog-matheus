cc2e9207d42d1f306068c07d07ace57afa46884d
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-05-29 17:25:11 -0300

    php

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -11,6 +11,15 @@ function inserirUsuario($conexao, $nome, $email, $tipo, $senha){
 mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
 
+function lerUsuarios($conexao){
+    $sql = "SELECT id, nome, tipo, email FROM usuarios";
 
+  $resultado =   mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+    
+    //retornamos resultado TRANSFORMADO em array associativo
+
+    return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
 
 ?>
