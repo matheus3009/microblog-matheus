79c6d3429f1e9f07f713d4d46d204980eb6e2200
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-06-05 17:23:34 -0300

    php

--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -43,11 +43,11 @@ $pagina = basename($_SERVER['PHP_SELF']);
             <li class="nav-item">
                 <a class="nav-link" href="meu-perfil.php">Meu perfil</a>
             </li>
-                       
+             <?php if($_SESSION['tipo'] == "admin") : ?>
             <li class="nav-item">
                 <a class="nav-link" href="usuarios.php">Usuários</a>
             </li>
-            
+            <?php endif; ?>
             <li class="nav-item">
                 <a class="nav-link" href="noticias.php">Notícias</a>
             </li>
