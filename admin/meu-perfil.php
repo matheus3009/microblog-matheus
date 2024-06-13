bdefc18267e428c434a5f8040567fbf9315f333a
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-06-13 16:30:12 -0300

    php ajustes

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -3,52 +3,43 @@ require_once "../inc/cabecalho-admin.php";
 require_once "../inc/funcoes-usuarios.php";
 
 
-/* 1) Carregue as funções de usuários */
 
-// Verificando se o usuário pode acessar esta página
+
 verificaAcesso();
 
-
-/* 2) Pegue o ID do usuário através da SESSÃO */
-/* Pegando o valor do parâmetro id vindo da URL */
-// Pegando o id do usuário logado
 $id = $_SESSION['id'];
 
 
-/* 3) Chame a função lerUmUsuario e guarde o que ela retorna (array de dados) */
+
 $dadosUsuario = lerUmUsuario($conexao, $id);
 
-/* 4) Programe uma condicional para detectar se o formulário de atualização foi acionado.
 
-		4.1) Capture os dados digitados no formulário (nome, e-mail) */
+
+		
 if(isset($_POST['atualizar'])){
 	$nome = $_POST['nome'];
 	$email = $_POST['email'];
 
-	// 4.2) Capture o tipo do usuário através da SESSÃO    
+	  
 	$tipo = $_SESSION['tipo'];
 	
-	/* Lógica para tratamento da senha 
-	Se o campo da senha estiver vazio OU se a senha digitada for a mesma já existente no banco, então significa que o usuário NÃO ALTEROU A SENHA. Portanto, devemos MANTER a senha
-	existente no banco.	*/
 
-	// 4.3) Faça a programação condicional necessária para a senha (é o mesmo código usado em usuario-atualiza.php)
+	
 	if( empty($_POST['senha']) || 
 		password_verify($_POST['senha'], $dadosUsuario['senha']) ){
 
-		// Manter a mesma senha (copiamos ela para uma variável)
+
 		$senha = $dadosUsuario['senha'];
 	} else {
-	/* Caso contrário, pegaremos a senha nova digitada e a CODIFICAREMOS ANTES de mandar/salvar no banco. */
+
 		$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 	}
 
-	// 4.4) Fora da condicional da senha, chame a função atualizarUsuario e passe os dados pra ela
 	atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
 	
-	/* 5) DESAFIO: faça com que, ao mudar o nome do usuário, automaticamente apareça o novo nome na index.php */
+
 	$_SESSION['nome'] = $nome;
-	// 4.5) Redirecione para a página index.php (a que está dentro de admin)
+
 	header("location:index.php");
 }
 
@@ -68,12 +59,12 @@ if(isset($_POST['atualizar'])){
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$dadosUsuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$dadosUsuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
